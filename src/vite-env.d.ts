1e3a283ceb572892370a2cd324b3f12c48bdff45
Tyler Robinson <tjrdesigner@gmail.com>
2025-04-28 23:07:02 -0700

    Fix deployment command in GitHub Actions workflows

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="vite/client" />
+
+// Add declarations for CSS Modules
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+// Add declarations for CSS files
+declare module '*.css';
+
+// Add declarations for SVG files
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
